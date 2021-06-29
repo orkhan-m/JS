41a792eb43e95cfc16b323834acd92aa20b206ef
orkhan-m <orkhanmirzayev96@gmail.com>
2021-06-29 23:15:51 +0200

    70. Project 1 document.querySelector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+// NOTE select element from HTML
+console.log(document.querySelector('.message')); //class are selected with . dot; id's are selected with # hash
+
+console.log(document.querySelector('.message').textContent);
