cc78d5dafc6114ca2f4d904dfbb31db8c7e1a2b8
orkhan-m <orkhanmirzayev96@gmail.com>
2021-06-30 22:21:03 +0200

    74. Implementing the Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -21,11 +21,47 @@
 
 // TOPIC 73. Handling CLick Events
 // addEventListener - listen for events (name of event click)
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   // guess is the value retrieved from gurss class
-  const guess = Number(console.log(document.querySelector('.guess').value));
+  const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number😡';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Right value 🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        'Your value is too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        'Your value is too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 }); // 'click'; 'dblclick'; 'mousedown'
+
+// git commit -m '74. Implementing the Game Logic'
