096822ece0171fe2ba669c8af5d271d935a8249f
orkhan-m <orkhanmirzayev96@gmail.com>
2021-06-30 22:30:26 +0200

    75. Manipulating CSS styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -25,14 +25,25 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector('.number').textContent = secretNumber;
+// 'click'; 'dblclick'; 'mousedown'                  !
 document.querySelector('.check').addEventListener('click', function () {
   // guess is the value retrieved from gurss class
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number😡';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Right value 🎉';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // when the number is higher
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
@@ -52,6 +63,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game';
       document.querySelector('.score').textContent = 0;
     }
+    // when the number is lower
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
@@ -62,6 +74,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-}); // 'click'; 'dblclick'; 'mousedown'
+});
 
-// git commit -m '74. Implementing the Game Logic'
+// git commit -m '75. Manipulating CSS styles'
