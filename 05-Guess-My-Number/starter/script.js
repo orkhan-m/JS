5c6944a3e6c9b2c4b70adf15369f324e42c6cbf1
orkhan-m <orkhanmirzayev96@gmail.com>
2021-06-30 20:54:44 +0200

    77. Select and Manipulate elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,3 +4,17 @@
 console.log(document.querySelector('.message')); //class are selected with . dot; id's are selected with # hash
 
 console.log(document.querySelector('.message').textContent);
+
+// TOPIC 77. Select and manipulate elements
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// . is the class selector
+console.log(document.querySelector('.guess').value);
+// with the input field to get actual avlue we use .value
+// NOTE only input elements has value
+document.querySelector('.guess').value = 23;
+// now instead of empty field it will be 23
+console.log(document.querySelector('.guess').value);
