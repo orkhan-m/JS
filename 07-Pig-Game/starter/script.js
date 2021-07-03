ad75afb7be9dc3003be7c770b2faf58b8c32122d
orkhan-m <orkhanmirzayev96@gmail.com>
2021-07-03 21:19:07 +0200

    81. Project 3. PIG GAME

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting consition
+score0El.textCOntent = 0;
+score1El.textContent = 0;
+
+// NOTE Hidden element added to CSS file
+diceEl.classList.add('hidden');
